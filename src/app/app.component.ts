94188153a9029d38b074631e07e9b7f02aad09c2
unknown <josue@tec-software.com>
2024-12-18 22:39:14 -0300

    ADD, directivas estruturales y listado de videojuegos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Videojuego } from './models';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,53 @@ export class AppComponent {
   estudiantes = ['Goku', 'Trunks', 'Gohan'];
   estadoEnvio: 'pendiente' | 'entregado' | 'rechazado' = 'pendiente';
   cargando = false;
+  hoy = new Date();
+  nombreUsuario = 'Josue';
+  porcentaje = 0.9;
+  pokemon = {
+    nombre: 'Pikachu',
+    nivel: 99,
+  };
+  precioPS5 = 9999;
+
+  carrito: string[] = [];
+  videojuegos: Videojuego[] = [
+    {
+      nombre: 'Resident Evil 3',
+      lanzamiento: new Date('1999-04-04'),
+      soldout: true,
+    },
+    {
+      nombre: 'Age of Empires II',
+      lanzamiento: new Date('1998-03-01'),
+      soldout: false,
+    },
+    {
+      nombre: 'GTA V',
+      lanzamiento: new Date('2013-09-17'),
+      soldout: true,
+    },
+  ];
 
   constructor() {
     this.cargando = true;
     setTimeout(() => {
       this.cargando = false;
     }, 3000);
-
     // setInterval(() => {
     //   this.hayError = !this.hayError;
     // }, 1000);
+  }
+
+  onClick(event: MouseEvent): void {
+    console.log('Click!', event);
+  }
+
+  agregarAlCarrito(videojuego: Videojuego) {
+    if (videojuego.soldout) {
+      alert('No hay stock!');
+    } else {
+      this.carrito.push(videojuego.nombre);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ResaltadoDirective
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ResaltadoDirective],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
